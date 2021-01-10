64e50667307f0eafe1aa577c8371f33b09daaa79
mercea violeta <consilierjuridic.violeta@gmail.com>
2021-01-10 16:01:00 +0200

    cleanup

---
+++ functions.js
@@ -0,0 +1,13 @@
+function showSkillsPage() {
+    var home = document.getElementById('home');
+    home.style.display = 'none';
+    var page = document.getElementById('skills');
+    page.style.display = '';
+}
+
+function showLanguagesPage() {
+    var skills = document.getElementById('skills');
+    skills.style.display = 'none';
+    var page = document.getElementById('languages');
+    page.style.display = '';
+}
